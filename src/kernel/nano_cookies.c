35f0d5f03e7f2ad3e5dd6665b8effe4e241506be
Alexander Bakst <abakst@cs.ucsd.edu>
2012-11-07 18:23:19 -0800

    make nano_cookies example 'better.' line 37 of nano_cookies.c is annoying - why cant I move the conditional outside the loop?

--- src/kernel/nano_cookies.c
+++ src/kernel/nano_cookies.c
@@ -1,19 +1,52 @@
 #include <csolve.h>
 #include <stdlib.h>
 
+/* struct tab { */
+/*   char NULLTERMSTR * REF(? DOMAIN([V;V])) STRINGPTR VALIDPTR LOC(D) domain; */
+/*   int REF(? DOMAIN([V; domain])) fd; */
+/* }; */
+
 struct tab {
-  char NULLTERMSTR * REF(? DOMAIN([V;V])) STRINGPTR VALIDPTR LOC(D) domain;
-  int REF(? DOMAIN([V; domain])) fd;
+  char NULLTERMSTR REF(DOMAIN_FN([VVADDR]) = DOMAIN_FN([V])) * STRINGPTR VALIDPTR LOC(D) domain;
+  int REF(DOMAIN_FN([V]) = DOMAIN_FN([domain])) fd;
 };
 
-/* extern struct tab INST(D,D) tabs[10] OKEXTERN; */
-extern struct tab INST(D,D) tabs[10] OKEXTERN;
+struct tab INST(D,D) * NNSTART NNVALIDPTR NNROOM_FOR(struct tab)
+                     * ARRAY NNSTART NNVALIDPTR NNSIZE_GE(40) tabs CHECK_TYPE;
 
-void change_domain2(char NULLTERMSTR * LOC(D) STRINGPTR dom)
-CHECK_TYPE GLOBAL(D)
+void change_domain()
 {
-  tabs[0].domain = "foo";
-  //  tabs[0].domain = "ASDF";
+  char *x;
+  if (tabs) {
+    if (tabs[0] != NULL) {
+      tabs[0]->fd = nondet();
+    }
+  }
+}
+
+extern struct tab INST(D,D) * new_tab(char NULLTERMSTR * STRINGPTR domain, int fd) OKEXTERN;
+
+void
+init_tabs()
+{
+  int i;
+  struct tab *t;
+  for (i = 0; i < 10; i++) {
+    t = new_tab("domain", nondet());
+    validptr(t);
+    if (tabs) {
+      tabs[i] = t;
+    }
+  }
+}
+
+int
+main(int  argc, char **argv) CHECK_TYPE
+{
+  tabs = malloc(10*sizeof(*tabs));
+  validptr(tabs);
+  init_tabs();
+  change_domain();
 }
 
   
